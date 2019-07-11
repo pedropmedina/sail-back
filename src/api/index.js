b7cea76759d7666a093ea847c35d78b7fb3b274f
pedropmedina <pedrodelilia@gmail.com>
2019-07-11 15:59:22 -0400

    Handle authorization via websocket

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server');
 const merge = require('lodash/merge');
 
 // api
@@ -11,19 +12,34 @@ const getCurrentUser = require('../utils/getCurrentUser');
 module.exports = {
   typeDefs: [pin.typeDefs, comment.typeDefs, user.typeDefs].join(' '),
   resolvers: merge({}, pin.resolvers, comment.resolvers, user.resolvers),
-  context: async ({ req }) => {
+  subscriptions: {
+    onConnect: async connectionParams => {
+      const auth = connectionParams.authorization || '';
+      if (auth) {
+        const token = auth.split(' ')[1];
+        const currentUser = await getCurrentUser(token, user.model.User);
+        return { currentUser };
+      }
+      throw new AuthenticationError('Missing authorization token');
+    }
+  },
+  context: async ({ req, connection }) => {
     let currentUser = null;
+    let token = null;
     try {
-      // get the user token from the headers
-      const auth = req.headers.authorization || '';
-      const token = auth ? auth.split(' ')[1] : null;
-      // if token, find user, else user === null
-      currentUser = token && (await getCurrentUser(token, user.model.User));
+      if (connection) {
+        // get currentUser over websocket
+        currentUser = connection.context.currentUser;
+      } else {
+        // get the token over http
+        const auth = req.headers.authorization || '';
+        token = auth && auth.split(' ')[1];
+        currentUser = token && (await getCurrentUser(token, user.model.User));
+      }
     } catch (error) {
       console.log('Error while getting current user!', error);
       throw error;
     }
-
     return {
       currentUser,
       models: {
