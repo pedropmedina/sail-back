e34aeceebaefef3b0562fd6a6bd15499154a0580
pedropmedina <pedrodelilia@gmail.com>
2019-07-18 22:34:05 -0400

    Fix issue triggering pre hook on save new users

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -47,20 +47,15 @@ const blacklistedTokenSchema = new Schema({
   token: { type: String, required: true, unique: true }
 });
 
-const User = mongoose.model('User', userSchema);
-const BlacklistedToken = mongoose.model(
-  'BlacklistedToken',
-  blacklistedTokenSchema
-);
-
 // middleware in charge of removing corresponding pins to removed user
 // use pin.remove to trigger 'pre' hook upon removal of pin and delete all comments corresponding to each pin
 userSchema.pre('remove', async function() {
   try {
     const pins = await Pin.find({ author: this._id });
-    return await Promise.all(pins.map(pin => pin.remove()));
+    await Promise.all(pins.map(pin => pin.remove()));
   } catch (error) {
     console.error('Error while deleting user!');
+    throw error;
   }
 });
 
@@ -68,10 +63,16 @@ userSchema.pre('save', function(next) {
   if (this.isNew) {
     const username = this.email.split('@')[0];
     this.username = username;
-    console.log('saved for the first time!!!!');
   }
   next();
 });
+
+const User = mongoose.model('User', userSchema);
+
+const BlacklistedToken = mongoose.model(
+  'BlacklistedToken',
+  blacklistedTokenSchema
+);
 
 module.exports = {
   User,
